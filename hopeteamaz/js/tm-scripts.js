25f79b7ffd42f8758dd114ca71bb3ad4cd07cae4
i-can <caltoonme2@gmail.com>
2016-05-09 17:07:22 +0530

    Add files via upload
    initial process

---
+++ hopeteamaz/js/tm-scripts.js
@@ -0,0 +1,250 @@
+function include(scriptUrl) {
+    document.write('<script src="' + scriptUrl + '"></script>');
+}
+
+function isIE() {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+};
+
+/* cookie.JS
+ ========================================================*/
+include('js/jquery.cookie.js');
+
+/* Easing library
+ ========================================================*/
+include('js/jquery.easing.1.3.js');
+
+
+/* Stick up menus
+ ========================================================*/
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+        include('js/tmstickup.js');
+
+        $(document).ready(function () {
+            $('#stuck_container').TMStickUp({})
+        });
+    }
+})(jQuery);
+
+/* ToTop
+ ========================================================*/
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+        include('js/jquery.ui.totop.js');
+
+        $(document).ready(function () {
+            $().UItoTop({easingType: 'easeOutQuart'});
+        });
+    }
+})(jQuery);
+
+/* EqualHeights
+ ========================================================*/
+;
+(function ($) {
+    var o = $('[data-equal-group]');
+    if (o.length > 0) {
+        include('js/jquery.equalheights.js');
+    }
+})(jQuery);
+
+
+
+/* Copyright Year
+ ========================================================*/
+;
+(function ($) {
+    var currentYear = (new Date).getFullYear();
+    $(document).ready(function () {
+        $("#copyright-year").text((new Date).getFullYear());
+    });
+})(jQuery);
+
+
+
+
+;
+(function ($) {
+    function include(url) {
+	document.write('<script src="js/' + url + '"></script>');
+	return false;
+}
+	include('superfish.js');
+	jQuery(function () {
+})
+})(jQuery);
+
+
+
+/* Responsive Tabs
+ ========================================================*/
+;
+(function ($) {
+    var o = $('.resp-tabs');
+    if (o.length > 0) {
+        include('js/jquery.responsive.tabs.js');
+
+        $(document).ready(function () {
+            o.easyResponsiveTabs();
+        });
+    }
+})(jQuery);
+
+
+
+/* Navbar
+ ========================================================*/
+;
+(function ($) {
+    include('js/jquery.rd-navbar.js');
+})(jQuery);
+
+
+
+
+/* JQuery UI Accordion
+ ========================================================*/
+;
+(function ($) {
+    var o = $('.accordion');
+    if (o.length > 0) {
+        include('js/jquery.ui.accordion.min.js');
+
+        $(document).ready(function () {
+            o.accordion({
+                active: false,
+                header: '.accordion_header',
+                heightStyle: 'content',
+                collapsible: true
+            });
+        });
+    }
+})(jQuery);
+
+/* Orientation tablet fix
+ ========================================================*/
+$(function () {
+    // IPad/IPhone
+    var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+        ua = navigator.userAgent,
+
+        gestureStart = function () {
+            viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";
+        },
+
+        scaleFix = function () {
+            if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+                viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+                document.addEventListener("gesturestart", gestureStart, false);
+            }
+        };
+
+    scaleFix();
+    // Menu Android
+    if (window.orientation != undefined) {
+        var regM = /ipod|ipad|iphone/gi,
+            result = ua.match(regM);
+        if (!result) {
+            $('.sf-menus li').each(function () {
+                if ($(">ul", this)[0]) {
+                    $(">a", this).toggle(
+                        function () {
+                            return false;
+                        },
+                        function () {
+                            window.location.href = $(this).attr("href");
+                        }
+                    );
+                }
+            })
+        }
+    }
+});
+var ua = navigator.userAgent.toLocaleLowerCase(),
+    regV = /ipod|ipad|iphone/gi,
+    result = ua.match(regV),
+    userScale = "";
+if (!result) {
+    userScale = ",user-scalable=0"
+}
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0' + userScale + '">');
+
+/* Camera
+========================================================*/
+;(function ($) {
+var o = $('#camera');
+    if (o.length > 0) {
+        if (!(isIE() && (isIE() < 9))) {
+            include('js/jquery.mobile.customized.min.js');
+        }
+
+        include('js/camera.js');
+
+        $(document).ready(function () {
+            o.camera({
+                autoAdvance: false,
+                height: '29.75609756097561%',
+                minHeight: '350px',
+                pagination: true,
+                thumbnails: false,
+                playPause: false,
+                hover: false,
+                loader: 'none',
+                navigation: false,
+                navigationHover: false,
+                mobileNavHover: false,
+                fx: 'simpleFade'
+            })
+        });
+    }
+})(jQuery);
+
+
+/* Contact Form
+ ========================================================*/
+;
+(function ($) {
+    var o = $('#contact-form');
+    if (o.length > 0) {
+        include('js/modal.js');
+        include('js/TMForm.js'); 
+
+        if($('#contact-form .recaptcha').length > 0){
+            include('//www.google.com/recaptcha/api/js/recaptcha_ajax.js');
+        }
+    }
+})(jQuery);
+
+
+
+
+
+
+/* FancyBox
+========================================================*/
+;(function ($) {
+    var o = $('.thumb');
+    if (o.length > 0) {
+        include('js/jquery.fancybox.js');
+        include('js/jquery.fancybox-media.js');
+        include('js/jquery.fancybox-buttons.js');
+        $(document).ready(function () {
+            o.fancybox();
+        });
+    }
+})(jQuery);
+
+/* Parallax
+=============================================*/
+;(function ($) {
+    include('js/jquery.rd-parallax.js');
+})(jQuery); 
+
+
+
